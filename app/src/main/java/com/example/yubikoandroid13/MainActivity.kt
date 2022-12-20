ef827b791cc016778f93d7b7ea5ab14d83086aa1
Marcin Michalek <marcin.michalek@miquido.com>
2022-12-20 09:44:20 +0100

    Add minimal bug reproduction code

--- app/src/main/java/com/example/yubikoandroid13/MainActivity.kt
+++ app/src/main/java/com/example/yubikoandroid13/MainActivity.kt
@@ -1,12 +1,35 @@
 package com.example.yubikoandroid13
 
-import androidx.appcompat.app.AppCompatActivity
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
+import com.yubico.yubikit.android.ui.OtpActivity
 
 class MainActivity : AppCompatActivity() {
+
+    private val scanYubikeyResult = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult()
+    ) {
+        if (it.resultCode == Activity.RESULT_OK) {
+            val otp = it.data?.getStringExtra(OtpActivity.EXTRA_OTP)
+            Log.d("OTP", "Scan succeeded: %s".format(otp))
+        } else {
+            Log.d("OTP", "Scan cancelled")
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findViewById<Button>(R.id.button).setOnClickListener {
+            scanYubikeyResult.launch(
+                Intent(this, OtpActivity::class.java)
+            )
+        }
     }
 }
